00434be76467455a4720afc0a7f31b077d2a98b2
Allen Luo <movoin@gmail.com>
2020-03-03 21:27:54 +0800

    update

--- conf/provision/entrypoint.d/00-composer.sh
+++ conf/provision/entrypoint.d/00-composer.sh
@@ -3,5 +3,9 @@
  ##
 source "$DOCKER_CONF_PATH/bin/functions.sh"
 
+if [ ! -d "$APP_PATH/.composer/" ];then
+	mkdir "$APP_PATH/.composer/"
+fi
+
 copyFileTo "$DOCKER_CONF_PATH/etc/composer/config.json" "$APP_PATH/.composer/config.json"
 chown -R $APP_USER:$APP_GROUP $APP_PATH/.composer
